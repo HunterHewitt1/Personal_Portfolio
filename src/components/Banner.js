e062c265bf5fe2d7f4b3d71bd03e78ed17ac3d24
Hunter Hewitt <thehunterhewitt@gmail.com>
2022-12-01 10:33:22 -0700

    Completed Navbar component & styling. Beginning banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+export const Banner = () => {
+  return (
+    <section className="banner" id="home">
+      <container>
+        <row className="align-items-center">
+          <col>
+            <span></span>
+          </col>
+        </row>
+      </container>
+    </section>
+  )
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from 'react'
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import { HashLink } from 'react-router-hash-link'
+import { BrowserRouter as Router } from 'react-router-dom'
+import logo from '../assets/img/logo.svg'
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
+import navIcon3 from '../assets/img/nav-icon3.svg'
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home')
@@ -25,7 +31,7 @@ export const NavBar = () => {
     <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
       <Container>
         <Navbar.Brand href="#home">
-          <img alt="Logo" />
+          <img src={logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"> </Navbar.Toggle>
         <span className="navbar-toggler-icon"></span>
@@ -68,20 +74,20 @@ export const NavBar = () => {
               Resume
             </Nav.Link>
           </Nav>
-          <span className="navbar-tex">
-            <div>
-              <a href="#LinkedIn">
-                <img src={'www.blank.com'} alt="" />
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#">
+                <img src={navIcon1} alt="" />
               </a>
-              <a href="#Instagram">
-                <img src={''} alt="" />
+              <a href="#">
+                <img src={navIcon2} alt="" />
               </a>
-              <a href="#Github">
-                <img src={''} alt="" />
+              <a href="#">
+                <img src={navIcon3} alt="" />
               </a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}>
-              <span>Let's Connect</span>
+              <span>Let’s Connect</span>
             </button>
           </span>
         </Navbar.Collapse>
